ce6cdb4bf935d275446aeb4a375431d00995c045
turbosnail <denisbatya@yandex.ru>
2015-04-30 07:54:07 +0300

    aston alfa 4

--- script.js
+++ script.js
@@ -124,9 +124,8 @@ $(function(){
             case 'path':
                 if(!startCircle)
                 {
-                    tempWaypoints = [];
-                    for(i in waypoints)
-                        tempWaypoints.push(waypoints[i]);
+                    tempWaypoints = clone(waypoints);
+
 
                     startCircle = drawPoint(true);
                 }
@@ -137,9 +136,7 @@ $(function(){
                         endCircle = drawPoint(true);
                         createPath(startCircle.id, endCircle.id);
 
-                        waypoints = [];
-                        for(i in tempWaypoints)
-                            waypoints.push(tempWaypoints[i]);
+                        waypoints = clone(tempWaypoints)
                     }
                     else
                     {
@@ -612,4 +609,36 @@ function save()
         function(){
 
     },'json');
+}
+
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 }
